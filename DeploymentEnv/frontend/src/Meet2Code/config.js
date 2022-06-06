8412655a32fba48f0349b3948f410a97b38e827a
exploring-curiosity <curiositywake@gmail.com>
2022-06-06 21:22:56 +0530

    changes https to wss

--- DeploymentEnv/frontend/src/Meet2Code/config.js
+++ DeploymentEnv/frontend/src/Meet2Code/config.js
@@ -1,6 +1,6 @@
 let serverEndpoint='https://m2c-acceptance.herokuapp.com';
 let clientEndpoint='https://meet2code.netlify.app';
-let wsEndpoint = 'https://m2c-yjs-acceptance.herokuapp.com';
+let wsEndpoint = 'wss://m2c-yjs-acceptance.herokuapp.com';
 let peerEndPoint = 'm2c-peer-acceptance.herokuapp.com';
 let peerPort = '443';
 let peerPath = '/peerjs';
